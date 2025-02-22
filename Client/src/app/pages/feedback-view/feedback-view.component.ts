74e5ea204867ccb45ed728416299bd0b82dd9a22
DaKili <42497623+DaKili@users.noreply.github.com>
2025-02-22 17:30:47 +0100

    granular approval/feedback

--- Client/src/app/pages/feedback-view/feedback-view.component.ts
+++ Client/src/app/pages/feedback-view/feedback-view.component.ts
@@ -7,10 +7,7 @@ import { FeedbackControllerService, ModuleVersionUpdateRequestDTO, FeedbackDTO, 
 import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
-import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
 import { HlmSelectModule } from '@spartan-ng/ui-select-helm';
-import { HlmSeparatorDirective } from '@spartan-ng/ui-separator-helm';
-import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
 import { toast } from 'ngx-sonner';
 
 import { BrnAlertDialogContentDirective, BrnAlertDialogTriggerDirective } from '@spartan-ng/ui-alertdialog-brain';
@@ -25,6 +22,9 @@ import {
   HlmAlertDialogTitleDirective
 } from '@spartan-ng/ui-alertdialog-helm';
 import { HttpErrorResponse } from '@angular/common/http';
+import { lucideCheck, lucideX } from '@ng-icons/lucide';
+import { provideIcons } from '@ng-icons/core';
+import { HlmIconComponent } from "../../../spartan-components/ui-icon-helm/src/lib/hlm-icon.component";
 
 @Component({
   selector: 'app-feedback-view',
@@ -33,15 +33,11 @@ import { HttpErrorResponse } from '@angular/common/http';
     CommonModule,
     FormsModule,
     RouterModule,
-
     BrnSelectModule,
     BrnSeparatorModule,
     HlmButtonDirective,
     HlmInputDirective,
-    HlmScrollAreaComponent,
     HlmSelectModule,
-    HlmSeparatorDirective,
-    HlmToasterComponent,
     BrnAlertDialogTriggerDirective,
     BrnAlertDialogContentDirective,
     HlmAlertDialogComponent,
@@ -51,8 +47,10 @@ import { HttpErrorResponse } from '@angular/common/http';
     HlmAlertDialogDescriptionDirective,
     HlmAlertDialogCancelButtonDirective,
     HlmAlertDialogActionButtonDirective,
-    HlmAlertDialogContentComponent
-  ],
+    HlmAlertDialogContentComponent,
+    HlmIconComponent
+],
+  providers: [provideIcons({ lucideCheck, lucideX })],
   templateUrl: './feedback-view.component.html'
 })
 export class FeedbackViewComponent {
@@ -72,30 +70,56 @@ export class FeedbackViewComponent {
   feedbackReason: string = '';
   rejectionReason: string = '';
 
+  fieldStates: Record<string, { accepted: boolean | null }> = {};
+  fieldFeedback: Record<string, string> = {};
+
+  handleApprove(key: string) {
+    this.fieldStates[key] = { accepted: true };
+    this.fieldFeedback[key] = '';
+    this.feedbackForm.patchValue({
+      [`${key}Accepted`]: true,
+      [`${key}Feedback`]: ''
+    });
+  }
+
+  handleReject(key: string) {
+    this.fieldStates[key] = { accepted: false };
+    this.feedbackForm.patchValue({
+      [`${key}Accepted`]: false
+    });
+  }
+
+  updateFeedback(key: string, value: string) {
+    this.fieldFeedback[key] = value;
+    this.feedbackForm.patchValue({
+      [`${key}Feedback`]: value
+    });
+  }
+
   getModuleVersionProperty(key: keyof ModuleVersionUpdateRequestDTO): string | undefined {
     return this.moduleVersion ? this.moduleVersion[key]?.toString() : undefined;
   }
 
   moduleFields = [
-    { key: 'titleEng', label: 'Title (EN)' },
-    { key: 'levelEng', label: 'Level (EN)' },
-    { key: 'languageEng', label: 'Language (EN)' },
-    { key: 'frequencyEng', label: 'Frequency (EN)' },
+    { key: 'titleEng', label: 'Title' },
+    { key: 'levelEng', label: 'Level' },
+    { key: 'languageEng', label: 'Language' },
+    { key: 'frequencyEng', label: 'Frequency' },
     { key: 'credits', label: 'Credits' },
     { key: 'duration', label: 'Duration' },
     { key: 'hoursTotal', label: 'Total Hours' },
     { key: 'hoursSelfStudy', label: 'Self-Study Hours' },
     { key: 'hoursPresence', label: 'Presence Hours' },
-    { key: 'examinationAchievementsEng', label: 'Examination Achievements (EN)' },
-    { key: 'repetitionEng', label: 'Repetition (EN)' },
-    { key: 'recommendedPrerequisitesEng', label: 'Recommended Prerequisites (EN)' },
-    { key: 'contentEng', label: 'Content (EN)' },
-    { key: 'learningOutcomesEng', label: 'Learning Outcomes (EN)' },
-    { key: 'teachingMethodsEng', label: 'Teaching Methods (EN)' },
-    { key: 'mediaEng', label: 'Media (EN)' },
-    { key: 'literatureEng', label: 'Literature (EN)' },
-    { key: 'responsiblesEng', label: 'Responsibles (EN)' },
-    { key: 'lvSwsLecturerEng', label: 'Lecturer SWs (EN)' }
+    { key: 'examinationAchievementsEng', label: 'Examination Achievements' },
+    { key: 'repetitionEng', label: 'Repetition' },
+    { key: 'recommendedPrerequisitesEng', label: 'Recommended Prerequisites' },
+    { key: 'contentEng', label: 'Content' },
+    { key: 'learningOutcomesEng', label: 'Learning Outcomes' },
+    { key: 'teachingMethodsEng', label: 'Teaching Methods' },
+    { key: 'mediaEng', label: 'Media' },
+    { key: 'literatureEng', label: 'Literature' },
+    { key: 'responsiblesEng', label: 'Responsibles' },
+    { key: 'lvSwsLecturerEng', label: 'Lecturer SWs' }
   ] as const;
 
   constructor(route: ActivatedRoute) {
