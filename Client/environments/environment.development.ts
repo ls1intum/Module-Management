1fb785a37ce3d3d46461f92a2b484183603437a0
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2025-11-09 15:13:01 +0100

    add angular dev environment

---
+++ Client/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  redirect: 'http://localhost:4200',
+  serverUrl: 'http://localhost:8080/api',
+  keycloak: {
+    url: 'http://localhost:8081',
+    realm: 'module-management',
+    clientId: 'module-management'
+  }
+};
