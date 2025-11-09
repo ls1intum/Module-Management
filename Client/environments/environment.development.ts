e45eb42880e8e4782405248cd3bad677fc93c1e8
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2025-11-09 15:57:57 +0100

    small fix

--- Client/environments/environment.development.ts
+++ Client/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   redirect: 'http://localhost:4200',
-  serverUrl: 'http://localhost:8080/api',
+  serverUrl: 'http://localhost:8080',
   keycloak: {
     url: 'http://localhost:8081',
     realm: 'module-management',
