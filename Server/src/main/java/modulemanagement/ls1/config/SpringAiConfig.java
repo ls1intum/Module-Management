de3f79ba1f492db974fd41f28882708c6e2d3588
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2025-12-29 17:05:32 +0100

    adjustments

--- Server/src/main/java/modulemanagement/ls1/config/SpringAiConfig.java
+++ Server/src/main/java/modulemanagement/ls1/config/SpringAiConfig.java
@@ -16,8 +16,11 @@ public class SpringAiConfig {
 
     private static final Logger log = LoggerFactory.getLogger(SpringAiConfig.class);
 
-    @Value("${USE_LOCAL_LLM:false}")
-    private boolean useLocalLLM;
+    @Value("${CHAT_MODEL_SOURCE}")
+    private String chatModelSource;
+
+    @Value("${EMBEDDING_MODEL_SOURCE}")
+    private String embeddingModelSource;
 
     @Primary
     @Bean
@@ -27,30 +30,12 @@ public class SpringAiConfig {
             return null;
         }
 
-        for (ChatModel model : chatModels) {
-            log.info("Found Chat Model: {} with temperature: {}",
-                    model.getDefaultOptions().getModel(),
-                    model.getDefaultOptions().getTemperature());
+        ChatModel selectedModel = chatModels.stream()
+                .filter(model -> model.getClass().getSimpleName().equalsIgnoreCase(chatModelSource))
+                .findFirst()
+                .orElse(chatModels.get(0));
 
-        }
-
-        ChatModel selectedModel;
-        if (useLocalLLM) {
-            selectedModel = chatModels.stream()
-                    .filter(model -> !model.getClass().getSimpleName().contains("Azure"))
-                    .findFirst()
-                    .orElse(chatModels.getFirst());
-            log.info("USE_LOCAL_LLM=true: Using OpenAI ChatModel: {} as primary",
-                    selectedModel.getClass().getSimpleName());
-        } else {
-            selectedModel = chatModels.stream()
-                    .filter(model -> model.getClass().getSimpleName().contains("Azure"))
-                    .findFirst()
-                    .orElse(chatModels.getFirst());
-            log.info("USE_LOCAL_LLM=false: Using Azure OpenAI ChatModel: {} as primary",
-                    selectedModel.getClass().getSimpleName());
-        }
-
+        log.info("Using ChatModel: {} as primary", selectedModel.getClass().getSimpleName());
         return selectedModel;
     }
 
@@ -62,26 +47,12 @@ public class SpringAiConfig {
             return null;
         }
 
-        for (EmbeddingModel model : embeddingModels) {
-            log.info("Found Embedding Model: {}", model.getClass().getSimpleName());
-        }
+        EmbeddingModel selectedModel = embeddingModels.stream()
+                .filter(model -> model.getClass().getSimpleName().equalsIgnoreCase(embeddingModelSource))
+                .findFirst()
+                .orElse(embeddingModels.get(0));
 
-        EmbeddingModel selectedModel;
-        if (useLocalLLM) {
-            selectedModel = embeddingModels.stream()
-                    .filter(model -> !model.getClass().getSimpleName().contains("Azure"))
-                    .findFirst()
-                    .orElse(embeddingModels.getFirst());
-            log.info("USE_LOCAL_LLM=true: Using OpenAI EmbeddingModel: {} as primary",
-                    selectedModel.getClass().getSimpleName());
-        } else {
-            selectedModel = embeddingModels.stream()
-                    .filter(model -> model.getClass().getSimpleName().contains("Azure"))
-                    .findFirst()
-                    .orElse(embeddingModels.getFirst());
-            log.info("USE_LOCAL_LLM=false: Using Azure OpenAI EmbeddingModel: {} as primary",
-                    selectedModel.getClass().getSimpleName());
-        }
+        log.info("Using EmbeddingModel: {} as primary", selectedModel.getClass().getSimpleName());
 
         return selectedModel;
     }
