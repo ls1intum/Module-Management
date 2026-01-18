e641612d49270506ebc9d59fe6440ada9b101ebe
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2026-01-18 19:35:53 +0100

    remove spartan and unused packages

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,11 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class', // Enable class-based dark mode
-  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './src/**/*.{html,ts}',
-    './src/spartan-components/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
