40f0bc719210ce9547bb54db881ca239cadcf525
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2026-02-01 15:20:22 +0100

    fix

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, effect, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 import { ToastModule } from 'primeng/toast';
-import { SecurityStore } from './core/security/security-store.service';
-import { isAdminRole, isProfessorRole, isReviewerRole } from './core/shared/user-role.utils';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +9,4 @@ import { isAdminRole, isProfessorRole, isReviewerRole } from './core/shared/user
   imports: [RouterOutlet, LayoutComponent, ToastModule],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  private router = inject(Router);
-  securityStore = inject(SecurityStore);
-  user = this.securityStore.user;
-
-  constructor() {
-    effect(() => {
-      if (!this.user()) return;
-      if (isAdminRole(this.user()!.role)) {
-        this.router.navigateByUrl('/admin');
-      } else if (isProfessorRole(this.user()!.role)) {
-        this.router.navigateByUrl('/proposals');
-      } else if (isReviewerRole(this.user()!.role) && this.user()!.userId) {
-        this.router.navigate(['/feedbacks/for-user', this.user()!.userId]);
-      }
-    });
-  }
-}
+export class AppComponent {}
--- Client/src/app/pages/index/index.component.ts
+++ Client/src/app/pages/index/index.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, effect, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { SecurityStore } from '../../core/security/security-store.service';
+import { isAdminRole, isProfessorRole, isReviewerRole } from '../../core/shared/user-role.utils';
 
 @Component({
   selector: 'index-component',
@@ -7,4 +9,21 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   templateUrl: './index.component.html'
 })
-export class IndexComponent {}
+export class IndexComponent {
+  private router = inject(Router);
+  securityStore = inject(SecurityStore);
+  user = this.securityStore.user;
+
+  constructor() {
+    effect(() => {
+      if (!this.user()) return;
+      if (isAdminRole(this.user()!.role)) {
+        this.router.navigateByUrl('/admin');
+      } else if (isProfessorRole(this.user()!.role)) {
+        this.router.navigateByUrl('/proposals');
+      } else if (isReviewerRole(this.user()!.role) && this.user()!.userId) {
+        this.router.navigate(['/feedbacks/for-user', this.user()!.userId]);
+      }
+    });
+  }
+}
