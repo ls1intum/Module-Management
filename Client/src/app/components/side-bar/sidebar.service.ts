bb63797f75603996bdb6817c1b1b096284007bc0
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2026-02-10 00:42:02 +0100

    adjustments

--- Client/src/app/components/side-bar/sidebar.service.ts
+++ Client/src/app/components/side-bar/sidebar.service.ts
@@ -1,10 +1,24 @@
-import { Injectable, signal } from '@angular/core';
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class SidebarService {
   private visible = signal(false);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
   readonly isVisible = this.visible.asReadonly();
+
+  constructor() {
+    this.router.events
+      .pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => this.hide());
+  }
 
   toggle(): void {
     this.visible.update((v) => !v);
