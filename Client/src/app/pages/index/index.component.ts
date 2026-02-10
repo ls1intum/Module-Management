c8dcc2b3f5bdda956e9a8a19bb7ec8f601da4d06
Mohamed Alaaser <mohamed.s.alaaser@icloud.com>
2026-02-10 02:27:35 +0100

    better home page

--- Client/src/app/pages/index/index.component.ts
+++ Client/src/app/pages/index/index.component.ts
@@ -1,30 +1,22 @@
-import { Component, effect, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { PanelModule } from 'primeng/panel';
 import { SecurityStore } from '../../core/security/security-store.service';
 import { isAdminRole, isProfessorRole, isReviewerRole } from '../../core/shared/user-role.utils';
 
 @Component({
   selector: 'index-component',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule, DividerModule, PanelModule],
   templateUrl: './index.component.html'
 })
 export class IndexComponent {
-  private router = inject(Router);
   securityStore = inject(SecurityStore);
   user = this.securityStore.user;
 
-  constructor() {
-    effect(() => {
-      if (!this.user()) return;
-      const roles = this.user()!.roles;
-      if (isAdminRole(roles)) {
-        this.router.navigateByUrl('/admin');
-      } else if (isProfessorRole(roles)) {
-        this.router.navigateByUrl('/proposals');
-      } else if (isReviewerRole(roles)) {
-        this.router.navigateByUrl('/feedbacks');
-      }
-    });
-  }
+  isAdmin = (): boolean => isAdminRole(this.user()?.roles);
+  isProfessor = (): boolean => isProfessorRole(this.user()?.roles);
+  isReviewer = (): boolean => isReviewerRole(this.user()?.roles);
 }
